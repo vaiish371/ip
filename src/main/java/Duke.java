5c0358f046fb30e218b42c84d00b4d4a50296018
vaiish371 <vaiish371@gmail.com>
2021-01-31 19:30:13 +0800

    Testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,5 @@
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n\nBye. Hope to see you again soon!");
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n\nBye. Hope to see you again soon!! :)");
     }
 }
