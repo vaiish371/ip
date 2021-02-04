3e703bef8cdd92cc9e43ae3b1e8e98a84825ab7b
vaiish371 <vaiish371@gmail.com>
2021-02-05 05:22:00 +0800

    Level 2 Attempt

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,13 +4,26 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n\n");
 
+        int taskCounter = 0;
         String line;
+        String[] tasks = new String[100];
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
 
         while(line.equals("bye") == false)
         {
-            System.out.println("        " + line);
+            if(line.equals("list")==true)
+            {
+                for(int j=0;j<taskCounter;++j)
+                {
+                    System.out.println("        "+ (j+1)+". "+tasks[j]);
+                }
+            }
+            else
+            {
+                tasks[taskCounter++]= line;
+                System.out.println("        added: " + line);
+            }
             line = in.nextLine();
         }
 
