53513fdacd1ef7257460f557090bae4a29c28642
vaiish371 <vaiish371@gmail.com>
2021-02-05 04:52:19 +0800

    Level 1 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n\nBye. Hope to see you again soon!! :)");
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n\n");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+
+        while(line.equals("bye") == false)
+        {
+            System.out.println("        " + line);
+            line = in.nextLine();
+        }
+
+        System.out.print("      Bye. Hope to see you again soon!");
     }
 }
