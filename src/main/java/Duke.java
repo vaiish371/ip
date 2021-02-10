b5b1601fb150ec197eed686fc7bd0eb1f1bd0480
vaiish371 <vaiish371@gmail.com>
2021-02-10 14:47:34 +0800

    Level 3 Commits

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,32 +1,79 @@
 import java.util.Scanner;
 
-public class Duke {
-    public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n\n");
 
-        int taskCounter = 0;
-        String line;
-        String[] tasks = new String[100];
-        Scanner in = new Scanner(System.in);
+public class Duke {
+    static String line,temp;
+    static int taskCounter=0;
+
+    public static final int MAX_TASK = 100;
+
+    static Task[] tasks = new Task[MAX_TASK];
+
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        printHorizontal();
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        printHorizontal();
+
         line = in.nextLine();
 
-        while(line.equals("bye") == false)
-        {
-            if(line.equals("list")==true)
-            {
-                for(int j=0;j<taskCounter;++j)
-                {
-                    System.out.println("        "+ (j+1)+". "+tasks[j]);
-                }
+        while(!(line.equals("bye"))) {
+            if(line.equals("list")) {
+               listTasks(tasks);
             }
-            else
-            {
-                tasks[taskCounter++]= line;
-                System.out.println("        added: " + line);
+            else if(line.contains("done ")){
+                markTasks(tasks,line.trim());
+            }
+            else {
+                tasks[taskCounter++] = new Task(line);
+                printHorizontal();
+                System.out.println("Added: "+ line);
+                printHorizontal();
             }
             line = in.nextLine();
         }
 
-        System.out.print("      Bye. Hope to see you again soon!");
+        printHorizontal();
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontal();
+    }
+
+    public static void listTasks(Task[] tasks){
+        printHorizontal();
+        if(taskCounter==0) {
+            System.out.println("No tasks are there.");
+        }
+        else {
+            System.out.println("Here are the tasks in your list:");
+            for (int j = 0; j < taskCounter; ++j) {
+                System.out.println("        " + (j + 1) + ".[" + tasks[j].getStatusIcon() + "] " + tasks[j].getDescription());
+            }
+        }
+        printHorizontal();
+    }
+
+    public static void markTasks(Task[] tasks,String userInput){
+
+        int taskIndex;
+        String taskNumAsString=userInput.substring(5);
+        int taskNumAsInt = Integer.parseInt(taskNumAsString);
+
+        printHorizontal();
+        if(!(taskNumAsInt>0 && taskNumAsInt<=taskCounter)){
+            System.out.println("Sorry! There are no such tasks!");
+        }
+        else{
+            taskIndex=taskNumAsInt-1;
+            tasks[taskIndex].markAsDone();
+            System.out.println("Nice! I've marked this task as done:"+System.lineSeparator()+
+                    "["+tasks[taskIndex].getStatusIcon()+"] "+tasks[taskIndex].getDescription());
+        }
+        printHorizontal();
+    }
+
+    public static void printHorizontal(){
+        String horizontalLine = "------------------------------------------";
+        System.out.println(horizontalLine);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task   {
+        private String description;
+        private boolean isDone;
+
+
+        public Task(String description)
+        {
+            this.description= description;
+            this.isDone = false;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public void markAsDone() {
+            isDone = true;
+        }
+
+    }
